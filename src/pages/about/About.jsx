ea105fae648061e57d4bc14b790c1f52e8eaa92d
RathodDeven <devenrathodrd@gmail.com>
2021-10-20 11:05:02 +0530

    added about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,70 @@
+import {sponsors, team, cards} from "./data"
+import './about.css'
+import image from '../../asset/images/logo.png'
+
+
+
+
+
 const About = () => {
     return (
-        <h1>About Page</h1>
+        <>
+            <div className="header">
+            <h1>Interality</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div className="card">
+                <h1>Learn about our Products</h1>
+                <div className="cardUrl">
+                    {cards.map((detail)=>(
+                            <div className="image" key = {detail.title}>
+                                <img src={detail.image} className="imgur" alt=""/>
+                                <div className="overlay">
+                                    <h2 className="imagetitle">{detail.title}</h2>
+                                    <p className="description">{detail.description}</p>
+                                </div>
+                            </div>
+                    ))}
+                </div>
+            </div>
+            <div className="card">
+                <h1>Learn about our Products</h1>
+                <div className="cardUrl">
+                    {cards.map((detail)=>(
+                            <div className="image" key = {detail.title}>
+                                <img src={detail.image} className="imgur" alt=""/>
+                                <div className="overlay">
+                                    <h2 className="imagetitle">{detail.title}</h2>
+                                    <p className="description">{detail.description}</p>
+                                </div>
+                            </div>
+                    ))}
+                </div>
+            </div>
+            <div className="partners">
+                <h1>Partners</h1>
+                <div className="partnerUrl">
+                    {sponsors.map((sponsor)=>(
+                            <a key={sponsor.name} className="partner" href={sponsor.link}>
+                                <img src={image} alt={sponsor.name} className="sponsorimg"/>     
+                            </a>
+                    ))}
+                </div>
+            </div>
+           <div className = "team">
+               <h1>Meet the Team</h1>
+               <div className="teamUrl">
+                    {team.map((human)=>(
+                            <a key={human.name} className="partner" href={human.link}>
+                                <img src={image} alt={human.name} className="teamimg" />     
+                            </a>
+                    ))}
+                </div>
+
+           
+
+           </div>
+        </>
     )
 }
 export default About;
---
+++ src/pages/about/data.js
@@ -0,0 +1,75 @@
+// Added Temporary Data as of now. 
+module.exports = {
+    sponsors:[
+        {
+                    name: 'interality',
+                    link: 'https://opensea.io/',
+                    image: '../../asset/images/logo.png',
+                },
+                {
+                    name: 'interality2',
+                    link: 'https://opensea.io/',
+                    image: '../../asset/images/logo.png',
+                },
+                {
+                    name: 'interality3',
+                    link: 'https://opensea.io/',
+                    image: '/logo.png',
+                },
+                {
+                    name: 'interality4',
+                    link: 'https://opensea.io/',
+                    image: '../../asset/images/logo.png',
+                },
+                {
+                    name: 'interality5',
+                    link: 'https://opensea.io/',
+                    image: '../../asset/images/logo',
+                }
+    ],
+    team: [
+        {
+            name: 'interality',
+            link: 'https://opensea.io/',
+            image: '../../asset/images/logo',
+        },
+        {
+            name: 'interality2',
+            link: 'https://opensea.io/',
+            image: '../../asset/images/logo',
+        },
+        {
+            name: 'interality3',
+            link: 'https://opensea.io//',
+            image: '../../asset/images/logo',
+        },
+        {
+            name: 'interality4',
+            link: 'https://opensea.io//',
+            image: '../../asset/images/logo',
+        },
+        {
+            name: 'interality5',
+            link: 'https://opensea.io/',
+            image: '../../asset/images/logo',
+        }
+    ],
+    cards:[
+        {
+            title: 'Metaverse',
+            description: 'A new Metaverse for the Internet',
+            image: 'https://visualise.com/wp-content/uploads/2017/09/Metaverse-1020x620-c-default.png',
+        },
+        {
+            title: 'Metaverse',
+            description: 'A new Metaverse for the Internet',
+            image: 'https://visualise.com/wp-content/uploads/2017/09/Metaverse-1020x620-c-default.png',
+        },
+        {
+            title: 'Metaverse',
+            description: 'A new Metaverse for the Internet',
+            image: 'https://visualise.com/wp-content/uploads/2017/09/Metaverse-1020x620-c-default.png',
+        }
+    ]
+}
+
